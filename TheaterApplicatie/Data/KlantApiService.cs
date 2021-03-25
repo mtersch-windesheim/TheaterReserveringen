8b5bb3d7c80ec92f8534bab7ff17af584bf7e940
Martijn ter Schegget <54362214+mtersch-windesheim@users.noreply.github.com>
2021-03-25 17:21:43 +0100

    Redelijk minimale KlantApiService.
    Implementeert interface IObjectService<Klant>, alleen zinnige code voor GetAll() (met nog een eigen HttpClient-object).

---
+++ TheaterApplicatie/Data/KlantApiService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TheaterApplicatie.Models;
+
+namespace TheaterApplicatie.Data
+{
+    public class KlantApiService : IObjectService<Klant>
+    {
+        private readonly HttpClient httpClient;
+        public KlantApiService()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:5001/");
+        }
+        public void Add(Klant klant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Klant Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Klant> GetAll()
+        {
+            string returnString = httpClient.GetAsync("/api/Klanten").Result.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<List<Klant>>(
+                returnString,
+                new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }
+            );
+        }
+
+        public bool Update(Klant klant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TheaterApplicatie/Startup.cs
+++ TheaterApplicatie/Startup.cs
@@ -25,7 +25,8 @@ namespace TheaterApplicatie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IObjectService<Klant>>(new KlantMemoryService());
+            //services.AddSingleton<IObjectService<Klant>>(new KlantMemoryService());
+            services.AddSingleton<IObjectService<Klant>>(new KlantApiService());
             services.AddControllersWithViews();
         }
 
