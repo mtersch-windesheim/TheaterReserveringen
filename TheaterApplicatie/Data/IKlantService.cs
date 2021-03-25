ea4b64ee796b4503fa8b231ecfe4a9686885378c
Martijn ter Schegget <54362214+mtersch-windesheim@users.noreply.github.com>
2021-03-25 16:21:05 +0100

    Specifieke interface IKlantService toegevoegd.
    Idee: implementatie bouwen, daarna generieke IService-interface toevoegen, en implementatie (triviaal) ombouwen naar IService<Klant>.

---
+++ TheaterApplicatie/Data/IKlantService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheaterApplicatie.Models;
+
+namespace TheaterApplicatie.Data
+{
+    interface IKlantService
+    {
+        List<Klant> GetAll();
+        Klant Get(int id);
+        void Add(Klant klant);
+        bool Update(Klant klant);
+        bool Delete(int id);
+        bool Exists(int id);
+    }
+}
